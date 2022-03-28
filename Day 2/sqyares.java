5283817550f2463286a92e6a53075b67c5f9b357
Prakhar Srivastava <prakharsh2422@gmail.com>
2022-03-28 22:35:41 +0530

    Add files via upload

---
+++ Day 2/sqyares.java
@@ -0,0 +1,10 @@
+package hackerrank;
+
+public class sqyares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day 2/stock.java
@@ -0,0 +1,10 @@
+package hackerrank;
+
+public class stock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
